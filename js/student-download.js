d8dcdce3ba88d08d6cc8112df242a58ca67c6a3a
majal <4157956+majal@users.noreply.github.com>
2023-06-05 21:47:56 +0800

    JSON formats and languages

--- js/student-download.js
+++ js/student-download.js
@@ -4,7 +4,7 @@ var DATASOURCE = [];
 window.addEventListener('DOMContentLoaded', () => {
 
   fetch( 
-    'data/2023_SLV.json?v=1.0.0-beta1.0.1', {
+    'data/2023_SLV.json?v=1.0.0-beta1.0.2', {
       method: "GET",
       mode: "same-origin", // ON PRODUCTION: same-origin. ON DEV: cors.
       // cache: "no-cache",
