fcb154eddfe95ee4bfbd6126f9b5bc52ba526628
majal <4157956+majal@users.noreply.github.com>
2024-01-30 20:54:14 +0800

    Maj GitHub Sync - Tuesday, 30 January, 2024 08:54:09 PM PST

--- js/student-download.js
+++ js/student-download.js
@@ -8,7 +8,7 @@ dates.startDate = Date.parse('2024-01-08T00:00:00.000+08:00');
 dates.endDate = Date.parse('2024-04-20T16:00:00.000+08:00');
 
 var languageData = 'data/2024_EN.json';
-var submitURL = 'https://www.majlovesreg.one/dynamic/pss/dl'
+var submitURL = 'https://pss.majlovesreg.one/dynamic/pss/dl'
 
 var LOCALE = 'en';
 var DATASOURCE = [];
