e0a98ba448821442590d06dae34536af40c86460
majal <4157956+majal@users.noreply.github.com>
2024-01-31 10:06:26 +0800

    Maj GitHub Sync - Wednesday, 31 January, 2024 10:06:22 AM PST

--- js/pss.js
+++ js/pss.js
@@ -1,9 +1,17 @@
-document.getElementById('bk').style.animationPlayState = 'paused';
-
-document.addEventListener('DOMContentLoaded', function() {
-   animateRun();
+document.addEventListener('DOMContentLoaded', () => {
+   document.getElementById('bk').style.animationPlayState = 'paused';
 }, false);
 
-function animateRun() {
-	document.getElementById('bk').style.animationPlayState = 'running';
-};
+window.addEventListener('load', () => {
+
+   document.getElementById('bk').style.animationPlayState = 'running';
+
+   window.setInterval( () => {
+
+   	let bk = document.getElementById('bk');
+		let bkclone = bk.cloneNode(true);
+		bk.parentNode.replaceChild(bkclone, bk);
+   
+   }, 1000 * 60 * 10 );
+
+}, false);
