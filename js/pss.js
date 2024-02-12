5dfcf5cc783ae3ca2678cea2e49b1115e799bb13
majal <4157956+majal@users.noreply.github.com>
2024-02-12 22:00:06 +0800

    Maj GitHub Sync - Monday, 12 February, 2024 10:00:00 PM PST

--- js/pss.js
+++ js/pss.js
@@ -4,19 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('load', () => {
 
-   // console.log( 'Initial load: ' + new Date() );
-
    document.getElementById('bk').style.animationPlayState = 'running';
 
    window.setInterval( () => {
-
-   	/*
-      console.log( 'Background image reload: ' + new Date() );
-
-   	let bk = document.getElementById('bk');
-   	let bkclone = bk.cloneNode(true);
-		bk.parentNode.replaceChild(bkclone, bk);
-      */
 
       location.href = location.origin + location.pathname + '?' + Math.random().toString(36).substring(2, 3) + '=' + Math.random().toString(36).substring(2);
    
